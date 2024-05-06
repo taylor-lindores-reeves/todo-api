c3a32f2169a07217237874846a56bc1c157a9797
Taylor Lindores Reeves <taylor-lindores@hotmail.co.uk>
2024-05-06 22:44:30 +0100

    Added create client validation

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,20 +1,29 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import type { Todo } from "@todo/app/api/v1/todos/route";
 import { TodoItem } from "@todo/components/todo-item";
 import { Input } from "@todo/components/ui/input";
 import { createTodo } from "@todo/lib/create-todo";
 import { useRouter } from "next/navigation";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import { z } from 'zod';
+
+const createTodoSchema = z.object({
+  name: z.string().min(1, { message: "Task name is required" }).max(50, { message: "Task name must be less than 55 characters" }),
+});
+
+type CreateTodoInputs = z.infer<typeof createTodoSchema>;
 
 export function TodoList({ todos }: { todos: Todo[]; }) {
   const router = useRouter();
-  const onSubmit: React.FormEventHandler<HTMLFormElement> = async (event) => {
-    event.preventDefault();
-    const form = event.currentTarget; // Get the form element from the event
-    const input = form.elements.namedItem('name') as HTMLInputElement; // Access the input directly if needed
 
-    await createTodo(input.value);
+  const form = useForm<CreateTodoInputs>({
+    resolver: zodResolver(createTodoSchema)
+  });
 
+  const onSubmit: SubmitHandler<CreateTodoInputs> = async (data) => {
+    await createTodo(data.name);
     form.reset();
     router.refresh();
   };
@@ -22,13 +31,16 @@ export function TodoList({ todos }: { todos: Todo[]; }) {
   return (
     <div className="border bg-white dark:bg-gray-800 shadow-lg rounded-lg p-8 w-full max-w-xl">
       <h1 className="text-2xl font-bold text-center mb-6 text-gray-900 dark:text-gray-100">Todo List</h1>
-      <form onSubmit={onSubmit} className="mb-4">
+      <form {...form} onSubmit={form.handleSubmit(onSubmit)} className="mb-4">
         <Input
-          name="name"
+          {...form.register("name")}
           className="w-full bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Add a new task..."
           type="text"
         />
+        {form.formState.errors.name && (
+          <p className="text-red-500 text-sm mt-2">{form.formState.errors.name.message}</p>
+        )}
       </form>
       <div className="space-y-4">
         {todos.map((todo) => (
